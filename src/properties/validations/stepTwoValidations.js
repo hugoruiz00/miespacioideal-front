2ef04a9742f5ff0df1a55d286d2b7664047be298
hugoruiz00 <hugo.ruiz.pz@gmail.com>
2024-08-16 22:37:21 -0600

    fix edit step two

--- src/properties/validations/stepTwoValidations.js
+++ src/properties/validations/stepTwoValidations.js
@@ -3,6 +3,6 @@ import yup from './messageValidations';
 export const stepTwoValidations = yup.object({
   description: yup.string().max(5000).required(),
   price: yup.number().required().positive().max(99999999.99),
-  paymentFrequencyId: yup.number().when('$isRent', ([isRent], schema) => isRent ? schema.required() : schema),
+  paymentFrequencyId: yup.number().when('$isRent', ([isRent], schema) => isRent ? schema.required() : schema.nullable()),
   numbers: yup.array().min(1).of(yup.number().transform((value, originalValue) => parseFloat(originalValue.value)).required()).required()
 });
